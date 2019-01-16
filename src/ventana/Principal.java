bfb1dc317f99d09b07a10934aa6e21df745092a1
Eneko Pérez Villanueva <eneko.perez@opendeusto.es>
2019-01-16 19:48:50 +0100

    a

--- src/ventana/Principal.java
+++ src/ventana/Principal.java
@@ -18,7 +18,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
-//eneko
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
